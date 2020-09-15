09823461d4b1f76428a63eb3091e12e5bebe99ef
Rasmus Andersson <rasmus@notion.se>
2020-09-14 17:09:51 -0700

    improves serving of the built-in js library; now using gzip compression when the http client accepts it and also precomputes the http response to simplify the reply

--- websocket.go
+++ websocket.go
@@ -1,13 +1,14 @@
 package gotalk
 
 import (
+	"bytes"
+	"compress/gzip"
 	"fmt"
-	"github.com/rsms/gotalk/js"
 	"golang.org/x/net/websocket"
-	"io"
 	"net/http"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 )
 
@@ -28,43 +29,113 @@ type WebSocketServer struct {
 const gotalkJSSuffix = "/gotalk.js"
 const gotalkJSMapSuffix = "/gotalk.js.map"
 
-func (s *WebSocketServer) serveResource(w http.ResponseWriter, r *http.Request, f func()) {
-	// serve javascript library
-	w.Header()["Cache-Control"] = []string{"public, max-age=300"}
-	etag := "\"" + gotalkjs.BrowserLibSHA1Base64 + r.URL.Path + "\""
-	w.Header()["ETag"] = []string{etag}
-	reqETag := r.Header["If-None-Match"]
+// js lib cache
+var (
+	jslibInitOnce sync.Once
+	jslibETag     string
+	jslibHeader   http.Header // alias map[string][]string
+	jslibLen      []string
+	jslibBody     []byte
+	jslibLenGzip  []string
+	jslibBodyGzip []byte
+)
 
-	if len(reqETag) != 0 && reqETag[0] == etag {
-		w.WriteHeader(http.StatusNotModified)
-	} else {
-		f()
+func init() {
+	jslibETag = "\"" + JSLibSHA1Base64 + "\""
+	jslibHeader = map[string][]string{
+		"Content-Type":  []string{"application/javascript; charset=utf-8"},
+		"Cache-Control": []string{"public,max-age=300"}, // 5min
+		"ETag":          []string{jslibETag},
 	}
+	// Note on Cache-Control:
+	// max-age is the max time a browser can hold on to a copy of gotalk.js without
+	// checking back with an etag. I.e. it does not limit the age of the cached resource
+	// but limits how often the browser revalidates. max-age should thus be relatively low
+	// as a revalidation request will in most cases end early with a HTTP 304 Not Modified
+	// response. 5min seems like a decent value.
+	// TODO: Consider making max-age configurable.
+
+	// uncompressed
+	jslibBody = []byte(JSLibString)
+	jslibLen = []string{strconv.FormatInt(int64(len(jslibBody)), 10)}
+
+	// gzip
+	var zbuf bytes.Buffer
+	zw, _ := gzip.NewWriterLevel(&zbuf, gzip.BestCompression)
+	_, err := zw.Write(jslibBody)
+	if err2 := zw.Close(); err2 != nil && err == nil {
+		err = err2
+	}
+	if err != nil {
+		panic(err)
+	}
+	jslibBodyGzip = zbuf.Bytes()
+	jslibLenGzip = []string{strconv.FormatInt(int64(len(jslibBodyGzip)), 10)}
+}
+
+func (s *WebSocketServer) maybeReplyNotModified(w http.ResponseWriter, r *http.Request) bool {
+	reqETag := r.Header["If-None-Match"]
+	if len(reqETag) != 0 && reqETag[0] == jslibETag {
+		w.WriteHeader(http.StatusNotModified)
+		return true
+	}
+	return false
+}
+
+func parseCommaStrSet(s string) map[string]struct{} {
+	m := make(map[string]struct{})
+	if len(s) == 0 {
+		return m
+	}
+	var start int = 0
+	var end int = -1
+	for i, b := range s {
+		if start == -1 {
+			if b != ' ' {
+				start = i
+			}
+		} else {
+			switch b {
+			case ' ':
+				end = i
+			case ',':
+				if end == -1 {
+					end = i
+				}
+				m[s[start:end]] = struct{}{}
+				end = -1
+				start = -1
+				break
+			}
+		}
+	}
+	if start != -1 {
+		end = len(s)
+		m[s[start:end]] = struct{}{}
+	}
+	return m
 }
 
 func (s *WebSocketServer) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	if strings.HasSuffix(r.URL.Path, gotalkJSSuffix) {
-		s.serveResource(w, r, func() {
-			w.Header()["Content-Type"] = []string{"text/javascript"}
-			wsPath := r.URL.Path[:len(r.URL.Path)-len(gotalkJSSuffix)+1]
-			serveURL := "this.gotalkResponderAt={ws:'" + wsPath + "'};"
-			sizeStr := strconv.FormatInt(int64(len(serveURL)+len(gotalkjs.BrowserLibString)), 10)
-			w.Header()["Content-Length"] = []string{sizeStr}
-			w.WriteHeader(http.StatusOK)
-			// Note: w conforms to interface { WriteString(string)(int,error) }
-			io.WriteString(w, serveURL)
-			io.WriteString(w, gotalkjs.BrowserLibString)
-		})
-	} else if strings.HasSuffix(r.URL.Path, gotalkJSMapSuffix) {
-		s.serveResource(w, r, func() {
-			w.Header()["Content-Type"] = []string{"application/json"}
-			w.Header()["Content-Length"] = []string{strconv.FormatInt(
-				int64(len(gotalkjs.BrowserLibSourceMapString)),
-				10,
-			)}
-			w.WriteHeader(http.StatusOK)
-			io.WriteString(w, gotalkjs.BrowserLibSourceMapString)
-		})
+		if s.maybeReplyNotModified(w, r) {
+			return
+		}
+		contentLength := jslibLen
+		body := jslibBody
+		header := w.Header()
+		acceptEncoding := parseCommaStrSet(r.Header.Get("Accept-Encoding"))
+		if _, ok := acceptEncoding["gzip"]; ok {
+			header["Content-Encoding"] = []string{"gzip"}
+			contentLength = jslibLenGzip
+			body = jslibBodyGzip
+		}
+		header["Content-Length"] = contentLength
+		for k, v := range jslibHeader {
+			header[k] = v
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(body)
 	} else {
 		// upgrade request connection to web socket protocol
 		s.Server.ServeHTTP(w, r)
