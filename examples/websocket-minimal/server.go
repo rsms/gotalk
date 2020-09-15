cf45598dec40008ce28bafb4b4ad94f971657c03
Rasmus Andersson <rasmus@notion.se>
2020-09-14 17:07:31 -0700

    improved examples/websocket-minimal

--- examples/websocket-minimal/server.go
+++ examples/websocket-minimal/server.go
@@ -6,12 +6,24 @@ import (
 	"net/http"
 )
 
+type Message struct {
+	Author string
+	Body   string
+}
+
 func main() {
-	gotalk.Handle("echo", func(in string) (string, error) {
-		return in, nil
+	// This function handles requests for "test/message".
+	gotalk.Handle("test/message", func(input string) (*Message, error) {
+		// It can return any Go type. Here we return a structure and no error.
+		return &Message{Author: "Bob", Body: input}, nil
 	})
+
+	// mount Gotalk at "/gotalk/"
 	http.Handle("/gotalk/", gotalk.WebSocketHandler())
+
+	// mount a file server to handle all other requests
 	http.Handle("/", http.FileServer(http.Dir(".")))
+
 	println("Listening on http://localhost:1234/")
 	panic(http.ListenAndServe("localhost:1234", nil))
 }
